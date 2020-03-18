c7294f7f4ea62a56ca33467c082052414721938a
Luiz Augusto Frambach <luiz_allen@hotmail.com>
2020-03-18 01:06:37 -0300

    funcionando docker file

--- src/main/java/com/project/transactionApi/TransactionApiApplication.java
+++ src/main/java/com/project/transactionApi/TransactionApiApplication.java
@@ -3,11 +3,13 @@ package com.project.transactionApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.*;
+
 @SpringBootApplication
 public class TransactionApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TransactionApiApplication.class, args);
+		run(TransactionApiApplication.class, args);
 	}
 
 }
